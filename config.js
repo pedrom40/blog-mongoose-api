46eb5e92b73db1f423ef34ef5e530847d0f3d42f
pedrom40 <pedro.morin1980@gmail.com>
2017-10-17 09:00:27 -0500

    update local db

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/blogPostsApp';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/blog_posts_app';
 exports.PORT = process.env.PORT || 8080;
